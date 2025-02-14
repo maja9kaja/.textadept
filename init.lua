73a8970e304563105e2bf4fa0788f166a67200c6
Maja <maja@disroot.org>
2025-02-14 19:34:48 +0100

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,45 @@
+-- Maja's ta config, some stuff come from orbitalquark's .textadept
+
+view.h_scroll_bar, view.v_scroll_bar = false, false
+buffer.tab_width = 2
+ui.find.highlight_all_matches = true
+
+textadept.editing.highlight_words = textadept.editing.HIGHLIGHT_SELECTED
+textadept.editing.auto_enclose = true
+local function set_strip_trailing_spaces()
+	textadept.editing.strip_trailing_spaces = buffer.lexer_language ~= 'diff'
+end
+
+
+require('spellcheck')
+require('file_diff')
+require('lua_repl')
+
+local lsp = require('lsp')
+lsp.server_commands.cpp = 'clangd'
+lsp.server_commands.zig = 'zls'
+
+-- VCS diff of current file.
+local m_file = textadept.menu.menubar[_L['File']]
+table.insert(m_file, #m_file - 1, {''}) -- before Quit
+table.insert(m_file, #m_file - 1, {
+	'VCS Diff', function()
+		local root = io.get_project_root()
+		if not buffer.filename or not root then return end
+		local diff
+		if lfs.attributes(root .. '/.hg') then
+			diff = os.spawn('hg diff "' .. buffer.filename .. '"', root):read('a')
+		elseif lfs.attributes(root .. '/.git') then
+			diff = os.spawn('git diff "' .. buffer.filename .. '"', root):read('a')
+		else
+			return
+		end
+		local buffer = buffer.new()
+		buffer:set_lexer('diff')
+		buffer:add_text(diff)
+		buffer:goto_pos(1)
+		buffer:set_save_point()
+	end
+})
+
+textadept.run.build_commands['build.zig'] = 'zig build'
